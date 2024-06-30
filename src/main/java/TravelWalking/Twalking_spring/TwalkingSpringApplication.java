caad662136451b67535022d3128ff4c4e417ec8a
1PSE <1tjdms0414@naver.com>
2024-06-30 21:34:48 +0900

    first commit

---
+++ src/main/java/TravelWalking/Twalking_spring/TwalkingSpringApplication.java
@@ -0,0 +1,13 @@
+package TravelWalking.Twalking_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwalkingSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwalkingSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/TravelWalking/Twalking_spring/TwalkingSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package TravelWalking.Twalking_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwalkingSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
